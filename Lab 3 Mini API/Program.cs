9ea85468f2df1da682da3e718542dea3d5be3741
seanie1995 <seanschelin@gmail.com>
2023-12-18 11:05:55 +0100

    Add project files.

---
+++ Lab 3 Mini API/Program.cs
@@ -0,0 +1,15 @@
+namespace Lab_3_Mini_API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var app = builder.Build();
+
+            app.MapGet("/", () => "Hello World!");
+
+            app.Run();
+        }
+    }
+}
